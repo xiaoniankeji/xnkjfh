cc1d046e924064c888d4915a06e93780c28e5c61
xnkeji <2517583153@qq.com>
2025-07-07 22:17:55 +0800

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -1,15 +1,1 @@
-<?php// 执行 cURL 请求
-$response = curl_exec($ch)or: ' . curl_error($ch);
-// 过滤掉关闭状态的接口
-$interfaces = json_decode($response, true);erface_status'] === 1;
-});
-
-// 重新编码为 JSON
-$filteredResponse = json_encode($activeInterfaces);
-
-// 设置响应头
-header('Content-Type: application/json');
-
-// 输出响应
-echo $filteredResponse;
-?>
+i
