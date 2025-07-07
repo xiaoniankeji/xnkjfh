aa17de0b72cefe3253e57f8865d4cf81ace7d161
xnkeji <2517583153@qq.com>
2025-07-07 16:24:37 +0800

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -1,29 +1,7 @@
-<?php
-// 目标 URL
-$targetUrl = 'https://fhjk.xnkeji.top/api-list.json';
-
-// 初始化 cURL
-$ch = curl_init();
-
-// 设置 cURL 选项
-curl_setopt($ch, CURLOPT_URL, $targetUrl);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-// 执行 cURL 请求
-$response = curl_exec($ch);
-
-// 检查是否有错误
-if (curl_errno($ch)) {
-    echo 'Curl error: ' . curl_error($ch);
-}
-
-// 关闭 cURL
-curl_close($ch);
-
+<?php// 执行 cURL 请求
+$response = curl_exec($ch)or: ' . curl_error($ch);
 // 过滤掉关闭状态的接口
-$interfaces = json_decode($response, true);
-$activeInterfaces = array_filter($interfaces, function($interface) {
-    return $interface['interface_status'] === 1;
+$interfaces = json_decode($response, true);erface_status'] === 1;
 });
 
 // 重新编码为 JSON
